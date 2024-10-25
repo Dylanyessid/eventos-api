d479dd4023909314d30552f75f569105ff445512
Dylanyessid <dylanyessid1904@gmail.com>
2024-10-25 18:02:59 -0500

    feat(auth): login endpoint with JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
-
+import {JwtModule} from "@nestjs/jwt"
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-    imports:[SequelizeModule.forFeature([User]), UsersModule],
+    imports:[SequelizeModule.forFeature([User]), UsersModule, 
+    JwtModule.registerAsync({
+        imports: [ConfigModule], // Importar ConfigModule para acceder a las variables de entorno
+        useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET'), // Cargar el secreto desde las variables de entorno
+            signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') }, // Cargar otras opciones si es necesario
+        }),
+        inject: [ConfigService],
+    })
+    ],
     controllers:[AuthController],
     providers:[AuthService, UsersService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,12 @@ import { User } from 'src/schemas/user.model';
 import { RegisterDTO } from './dto/register.dto';
 import { UsersService } from 'src/users/users.service';
 import {genSalt, hash, compare} from "bcrypt"
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
 
-    constructor( @InjectModel(User) private readonly userModel: typeof User ){ }
+    private readonly saltRounds = 10;
+    constructor( @InjectModel(User) private readonly userModel: typeof User, private readonly jwtService: JwtService, ){ }
 
     async createUser(newUserData: RegisterDTO): Promise<User | null>{
         const {name,email,password} = newUserData
@@ -43,15 +45,31 @@ export class AuthService {
         
     }
 
-
-    private readonly saltRounds = 10;
-    
+    async validateUser(email:string, password:string):Promise<Boolean | null>{
+        try {
+            const user = await this.userModel.findOne({
+                where:{
+                    email,
+                    deletedAt:null
+                },
+                attributes: ['email', 'password']
+            })
+            if(!user) return false
+            return this.comparePassword(password, user.password)
+        } catch (error) {
+            return null
+        }
+    }
     async hashPassword(password: string): Promise<string> {
         const salt = await genSalt(this.saltRounds);
         return await hash(password, salt);
-      }
+    }
     
-      async comparePassword(plainPassword: string,hashedPassword: string,
+     login(payload:object): string{
+        return this.jwtService.sign(payload)
+    }
+
+    private async comparePassword(plainPassword: string,hashedPassword: string,
       ): Promise<boolean> {
         return await compare(plainPassword, hashedPassword);
       }
