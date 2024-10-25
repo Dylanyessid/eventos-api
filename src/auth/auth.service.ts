09358656d85b91b002ce38b5f5f2a69270618ae5
Dylanyessid <dylanyessid1904@gmail.com>
2024-10-25 17:25:54 -0500

    refactor(auth): encrypt password in register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,16 +3,15 @@ import { InjectModel } from '@nestjs/sequelize';
 import { User } from 'src/schemas/user.model';
 import { RegisterDTO } from './dto/register.dto';
 import { UsersService } from 'src/users/users.service';
-
+import {genSalt, hash, compare} from "bcrypt"
 @Injectable()
 export class AuthService {
 
-    constructor( @InjectModel(User) private readonly userModel: typeof User ){
-       
-    }
+    constructor( @InjectModel(User) private readonly userModel: typeof User ){ }
 
     async createUser(newUserData: RegisterDTO): Promise<User | null>{
         const {name,email,password} = newUserData
+
         return this.userModel.build(
             {
                 name,
@@ -26,11 +25,36 @@ export class AuthService {
 
     async getAllUsers() : Promise<User[] | []> {
         
-     
-
         const a = await this.userModel.findAll()
         return []
     }
+
+    async checkIfUserExists(email:string): Promise<Boolean | null> {
+        try {
+            return Boolean(await this.userModel.findOne({
+                where:{
+                    email,
+                    deletedAt: null
+                }
+            }))
+        } catch (error) {
+            return null
+        }
+        
+    }
+
+
+    private readonly saltRounds = 10;
+    
+    async hashPassword(password: string): Promise<string> {
+        const salt = await genSalt(this.saltRounds);
+        return await hash(password, salt);
+      }
+    
+      async comparePassword(plainPassword: string,hashedPassword: string,
+      ): Promise<boolean> {
+        return await compare(plainPassword, hashedPassword);
+      }
 
    
 
