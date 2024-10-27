5f8063d6129133ea4b2f007e51d5ef5e4c6cc4ef
Dylanyessid <dylanyessid1904@gmail.com>
2024-10-27 12:21:39 -0500

    feat(auth): jwt guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    
+    constructor(private readonly usersService: UsersService){}
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':email')
+    async getUserById(@Param('email') email:string){
+        const user = await this.usersService.getUserByEmail(email)
+        return user
+    }
+}
