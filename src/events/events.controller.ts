4dda5652b35e3516c5df80c55c5fbbf6a6b6cd34
Dylanyessid <dylanyessid1904@gmail.com>
2024-11-05 22:18:45 -0500

    feat(events): create a new event

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateEventDTO } from './dto/createEvent.dto';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+
+    constructor(private readonly eventService:EventsService){
+
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('/')
+    @HttpCode(HttpStatus.CREATED)
+    async create (@Body() createEventDTO:CreateEventDTO){
+        const event = await this.eventService.createEvent(createEventDTO)
+        return event;
+    }
+
+}
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { Event } from 'src/schemas/event.model';
+import { UserRole } from 'src/schemas/userRoles.model';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports:[SequelizeModule.forFeature([Event, UserRole])],
   providers: [EventsService],
   controllers: [EventsController]
 })
